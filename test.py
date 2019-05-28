a4d7a0b3d6878eb6dc0acdb64f02bb103cdebcae
Unknown <415569252@qq.com>
2019-05-28 11:50:37 +0800

    update

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 from ImageSimilarity import *
 import os
 import cv2
+import time
 
 if __name__ == "__main__":
     judgement = FlowerSimilarityJudgement()
     judgement.testPrepare("./selection/lavender.jpeg")
     dataset = os.listdir("./dataset/")
     for image in dataset:
+        start = time.time()
         image = "./dataset/" + image
         score = judgement.test(image)
+        end = time.time()
+        print(str(end-start))
         if score > 0.56:
             print(image)
             img = cv2.imread(image)
             cv2.imshow("result", img)
             cv2.waitKey(0)
-            
